d40ccc3529853c43ad24b47dbf4cb0a2ccdfe3ce
alexbowen <alexbowen123@googlemail.com>
2022-03-14 16:53:25 +0000

    add page for maintenance mode

--- pages/maintenance.php
+++
@@ -1,6 +0,0 @@
-<section class="introduction content-section">
-  <div class="content-intro">
-    <h1>The website is currently offline for maintenance.</h1>
-    <p>This should only be for approx 2-3 hours. Please try again later.</p>  
-  </div>
-</section>
