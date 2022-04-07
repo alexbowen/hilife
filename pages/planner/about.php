4b5702f15736893d34d9e7e3d55c1c82e197da2e
alexbowen <alexbowen123@googlemail.com>
2022-04-07 03:17:23 +0100

    refine planner aboutpage layout

--- pages/planner/about.php
+++ pages/planner/about.php
@@ -6,13 +6,13 @@
 <section class="content-section">
   <h2>Music playlists</h2>
   <div class="content-border__container">
+    <img src="/assets/images/planner/top25.png" alt="" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
     <h3>Your top 25 tunes</h3>
-    <img src="/assets/images/planner/top25.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
     <p class="lead">These are the main tunes for the DJ to include on the night</p>
     <p class="clearfix">Bro ipsum dolor sit amet avie pipe phat Skate rig cruiser over the bars sharkbite back country. Snake bite hardtail huckfest wheels bowl trail swag.</p>
 
+    <img src="/assets/images/planner/spotify.png" alt="" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
     <h3>Use Spotify playlists</h3>
-    <img src="/assets/images/planner/spotify.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
     <p class="lead">You can link playlists from Spotify with your event</p>
     <p class="clearfix">Bro ipsum dolor sit amet avie pipe phat Skate rig cruiser over the bars sharkbite back country. Snake bite hardtail huckfest wheels bowl trail swag.</p>
   </div>
@@ -21,13 +21,13 @@
 <section class="content-section">
   <h2>Music themes</h2>
   <div class="content-border__container">
+    <img src="/assets/images/planner/categories.png" alt="" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
     <h3>Your prefered music categories</h3>
-    <img src="/assets/images/planner/categories.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
     <p class="lead">Categories of music you would like to be played on the night</p>
     <p class="clearfix">Bro ipsum dolor sit amet avie pipe phat Skate rig cruiser over the bars sharkbite back country. Snake bite hardtail huckfest wheels bowl trail swag.</p>
 
+    <img src="/assets/images/planner/decades.png" alt="" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
     <h3>Your prefered music decades</h3>
-    <img src="/assets/images/planner/decades.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
     <p class="lead">A guide to how contemporary/retro or focused on one or two decades to make the music</p>
     <p class="clearfix">Bro ipsum dolor sit amet avie pipe phat Skate rig cruiser over the bars sharkbite back country. Snake bite hardtail huckfest wheels bowl trail swag.</p>
   </div>
@@ -36,11 +36,11 @@
 <section class="content-section">
   <h2>Music policy</h2>
   <div class="content-border__container">
-    <img src="/assets/images/planner/policy.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
+    <img src="/assets/images/planner/policy.png" alt="" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
     <p class="lead">What you DON'T want to hear is as important as what you DO</p>
     <p class="clearfix"> Remember your guests will make requests on the night, so the DJ needs a guide to what you think is acceptable. There is an area to put banned songs, to set your requests policy, microphone use by the DJ & policy on cheese / guilty pleasures.</p>
 
-    <img src="/assets/images/planner/policy2.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
+    <img src="/assets/images/planner/policy2.png" alt="" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
     <p class="lead">Details of first dance & last song of the night & an area to include any additional song info</p>
     <p class="clearfix">Bro ipsum dolor sit amet avie pipe phat Skate rig cruiser over the bars sharkbite back country. Snake bite hardtail huckfest wheels bowl trail swag.</p>
   </div>
