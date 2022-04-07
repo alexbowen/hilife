6cc53965a0cde7fa4d98f0d4c703f84db62f7df6
alexbowen <alexbowen123@googlemail.com>
2022-04-07 02:18:56 +0100

    refine planner aboutpage layout

--- pages/planner/about.php
+++ pages/planner/about.php
@@ -3,36 +3,39 @@
 <p class="lead">Music planner intro text. Music planner intro text. Music planner intro text. Music planner intro text.</p>
 
 <section class="content-section">
-  <h2>Your top 25 tunes</h2>
-  <img src="/assets/images/planner/top25.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid rounded float-end" />
-  <p class="lead">These are the main tunes for the DJ to include on the night</p>
-  <p class="clearfix">Bro ipsum dolor sit amet avie pipe phat Skate rig cruiser over the bars sharkbite back country. Snake bite hardtail huckfest wheels bowl trail swag. ACL whip pow pow grom switch. Chain suck roadie free ride liftie McTwist backside laps core shot apres flow. 180 BB schwag face shots gapers. Ollie stoked corduroy schwag drop, bowl 360.</p>
-</section>
+  <h2>Music playlists</h2>
+  <div class="content-border__container">
+    <h3>Your top 25 tunes</h3>
+    <img src="/assets/images/planner/top25.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
+    <p class="lead">These are the main tunes for the DJ to include on the night</p>
+    <p class="clearfix">Bro ipsum dolor sit amet avie pipe phat Skate rig cruiser over the bars sharkbite back country. Snake bite hardtail huckfest wheels bowl trail swag. ACL whip pow pow grom switch. Chain suck roadie free ride liftie McTwist backside laps core shot apres flow. 180 BB schwag face shots gapers. Ollie stoked corduroy schwag drop, bowl 360.</p>
 
-<section class="content-section">
-  <h2>Your favourite music categories</h2>
-  <img src="/assets/images/planner/categories.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid rounded float-end" />
-  <p class="lead">Categories of music you would like to be played on the night</p>
-  <p class="clearfix">Bro ipsum dolor sit amet avie pipe phat Skate rig cruiser over the bars sharkbite back country. Snake bite hardtail huckfest wheels bowl trail swag. ACL whip pow pow grom switch. Chain suck roadie free ride liftie McTwist backside laps core shot apres flow. 180 BB schwag face shots gapers. Ollie stoked corduroy schwag drop, bowl 360.</p>
-</section>
-
-<section class="content-section">
-  <h2>Your favourite music decades</h2>
-  <img src="/assets/images/planner/decades.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid rounded float-end" />
-  <p class="lead">A guide to how contemporary/retro or focused on one or two decades to make the music</p>
-  <p class="clearfix">Bro ipsum dolor sit amet avie pipe phat Skate rig cruiser over the bars sharkbite back country. Snake bite hardtail huckfest wheels bowl trail swag. ACL whip pow pow grom switch. Chain suck roadie free ride liftie McTwist backside laps core shot apres flow. 180 BB schwag face shots gapers. Ollie stoked corduroy schwag drop, bowl 360.</p>
-</section>
-
-<section class="content-section">
-  <h2>Music policy at your event</h2>
-  <img src="/assets/images/planner/policy.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid rounded float-end" />
-  <p class="lead">What you DON'T want to hear is as important as what you DO</p>
-  <p class="clearfix"> Remember your guests will make requests on the night, so the DJ needs a guide to what you think is acceptable. There is an area to put banned songs, to set your requests policy, microphone use by the DJ & policy on cheese / guilty pleasures.</p>
-</section>
-
-<section class="content-section">
-  <h2>Use Spotify playlists</h2>
-  <img src="/assets/images/planner/spotify.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid rounded float-end" />
+  <h3>Use Spotify playlists</h3>
+  <img src="/assets/images/planner/spotify.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
   <p class="lead">You can link playlists from Spotify with your event</p>
   <p class="clearfix">Bro ipsum dolor sit amet avie pipe phat Skate rig cruiser over the bars sharkbite back country. Snake bite hardtail huckfest wheels bowl trail swag. ACL whip pow pow grom switch. Chain suck roadie free ride liftie McTwist backside laps core shot apres flow. 180 BB schwag face shots gapers. Ollie stoked corduroy schwag drop, bowl 360.</p>
+</div>
+</section>
+
+<section class="content-section">
+  <h2>Music themes</h2>
+<div class="content-border__container">
+  <h3>Your prefered music categories</h3>
+  <img src="/assets/images/planner/categories.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
+  <p class="lead">Categories of music you would like to be played on the night</p>
+  <p class="clearfix">Bro ipsum dolor sit amet avie pipe phat Skate rig cruiser over the bars sharkbite back country. Snake bite hardtail huckfest wheels bowl trail swag. ACL whip pow pow grom switch. Chain suck roadie free ride liftie McTwist backside laps core shot apres flow. 180 BB schwag face shots gapers. Ollie stoked corduroy schwag drop, bowl 360.</p>
+
+  <h3>Your prefered music decades</h3>
+  <img src="/assets/images/planner/decades.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
+  <p class="lead">A guide to how contemporary/retro or focused on one or two decades to make the music</p>
+  <p class="clearfix">Bro ipsum dolor sit amet avie pipe phat Skate rig cruiser over the bars sharkbite back country. Snake bite hardtail huckfest wheels bowl trail swag. ACL whip pow pow grom switch. Chain suck roadie free ride liftie McTwist backside laps core shot apres flow. 180 BB schwag face shots gapers. Ollie stoked corduroy schwag drop, bowl 360.</p>
+</div></section>
+
+<section class="content-section">
+  <h2>Music policy</h2>
+  <div class="content-border__container">
+  <img src="/assets/images/planner/policy.png" alt="we supply DJs for corporate events, like this one at Unity Works in Wakefield for the Beer Festival" height="200" width="450" class="ms-4 img-fluid img-thumbnail rounded float-end" />
+  <p class="lead">What you DON'T want to hear is as important as what you DO</p>
+  <p class="clearfix"> Remember your guests will make requests on the night, so the DJ needs a guide to what you think is acceptable. There is an area to put banned songs, to set your requests policy, microphone use by the DJ & policy on cheese / guilty pleasures.</p>
+</div>
 </section>
