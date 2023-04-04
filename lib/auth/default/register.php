abf584c3f5ea3ed183233074a6858f8a7604a6cd
alexbowen <alexbowen123@googlemail.com>
2023-04-04 12:54:30 +0100

    fix recaptcha logic for password reset

--- lib/auth/default/register.php
+++ lib/auth/default/register.php
@@ -18,45 +18,45 @@ if ($_POST['action'] == 'register') {
     // If the reCAPTCHA API response is valid 
     if($responseData->success){ 
 
-  require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
-  $auth = new \Delight\Auth\Auth($database->connection, null);
-  try {
-    $userId = $auth->register($_POST['email'], $_POST['password'], null, function ($selector, $token) {
+      require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
+      $auth = new \Delight\Auth\Auth($database->connection, null);
+      try {
+        $userId = $auth->register($_POST['email'], $_POST['password'], null, function ($selector, $token) {
 
-      $url = constant('BASE_URL') . '/auth/verify?selector=' . urlencode($selector) . '&token=' . urlencode($token);
+          $url = constant('BASE_URL') . '/auth/verify?selector=' . urlencode($selector) . '&token=' . urlencode($token);
 
-      Mailer::send(
-        $_POST["email"],
-        'Hi-Life Entertainment registration confirm email',
-        "Click this link to activate your account\n\n" . $url
-      );
+          Mailer::send(
+            $_POST["email"],
+            'Hi-Life Entertainment registration confirm email',
+            "Click this link to activate your account\n\n" . $url
+          );
 
-      Notify::add('message', 'Check your email to verify your account');
-  
-      header('Location: /');
-    });
-  }
-  catch (\Delight\Auth\InvalidEmailException $e) {
-      die('Invalid email address');
-  }
-  catch (\Delight\Auth\InvalidPasswordException $e) {
-      die('Invalid password');
-  }
-  catch (\Delight\Auth\UserAlreadyExistsException $e) {
-      Notify::add('error', 'User already exists');
+          Notify::add('message', 'Check your email to verify your account');
 
-      header('Location: /register');
-  }
-  catch (\Delight\Auth\TooManyRequestsException $e) {
-      die('Too many requests');
-  }
-}
+          header('Location: /');
+        });
+      }
+      catch (\Delight\Auth\InvalidEmailException $e) {
+          die('Invalid email address');
+      }
+      catch (\Delight\Auth\InvalidPasswordException $e) {
+          die('Invalid password');
+      }
+      catch (\Delight\Auth\UserAlreadyExistsException $e) {
+          Notify::add('error', 'User already exists');
+
+          header('Location: /register');
+      }
+      catch (\Delight\Auth\TooManyRequestsException $e) {
+          die('Too many requests');
+      }
+    }
   } else{
     Notify::add('error', 'Invalid Recaptcha form submission');
     header('Location: /');
   }
-  } else {
-  header("Location:".$_SERVER['HTTP_REFERER']);
-  }
+} else {
+header("Location:".$_SERVER['HTTP_REFERER']);
+}
 
 ?>
--- lib/auth/default/reset.php
+++ lib/auth/default/reset.php
@@ -19,39 +19,44 @@ if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'
    
   // If the reCAPTCHA API response is valid 
   if($responseData->success){ 
-if ($_POST['action'] == 'forgot') {
-  try {
-    $auth->forgotPassword($_POST['email'], function ($selector, $token) {
+    if ($_POST['action'] == 'forgot') {
+      try {
+        $auth->forgotPassword($_POST['email'], function ($selector, $token) {
 
-      $url = constant('BASE_URL') . '/auth/reset?action=reset&selector=' . \urlencode($selector) . '&token=' . \urlencode($token);
+          $url = constant('BASE_URL') . '/auth/reset?action=reset&selector=' . \urlencode($selector) . '&token=' . \urlencode($token);
 
-      Mailer::send(
-        $_POST["email"],
-        'Hi-Life Entertainment password reset',
-        "Click this link to reset your password\n\n" . $url
-      );
+          Mailer::send(
+            $_POST["email"],
+            'Hi-Life Entertainment password reset',
+            "Click this link to reset your password\n\n" . $url
+          );
 
-      Notify::add('message', 'Check your email for reset password link');
+          Notify::add('message', 'Check your email for reset password link');
 
-      header('Location: /');
-    });
-  }
-  catch (\Delight\Auth\InvalidEmailException $e) {
-    Notify::add('error', 'No email address registered for ' . $_POST['email']);
+          header('Location: /');
+        });
+      }
+      catch (\Delight\Auth\InvalidEmailException $e) {
+        Notify::add('error', 'No email address registered for ' . $_POST['email']);
 
+        header('Location: /');
+      }
+      catch (\Delight\Auth\EmailNotVerifiedException $e) {
+        Notify::add('error', 'Email not verified ' . $_POST['email']);
+
+        header('Location: /');
+      }
+      catch (\Delight\Auth\TooManyRequestsException $e) {
+        die('Too many requests');
+      }
+    }
+  } else {
+    Notify::add('error', 'Invalid Recaptcha form submission');
     header('Location: /');
   }
-  catch (\Delight\Auth\EmailNotVerifiedException $e) {
-    Notify::add('error', 'Email not verified ' . $_POST['email']);
-
-    header('Location: /');
-  }
-  catch (\Delight\Auth\TooManyRequestsException $e) {
-    die('Too many requests');
-  }
+} else {
+  header("Location:".$_SERVER['HTTP_REFERER']);
 }
-}
-
 
 if ($_GET['action'] == 'reset') {
   if ($auth->canResetPassword($_GET['selector'], $_GET['token'])) {
@@ -75,11 +80,5 @@ if ($_POST['action'] == 'new') {
   catch (\Delight\Auth\TokenExpiredException $e) {
     header('Location: /');
   }
-} else{
-  Notify::add('error', 'Invalid Recaptcha form submission');
-  header('Location: /');
-}
-} else {
-header("Location:".$_SERVER['HTTP_REFERER']);
 }
 ?>
